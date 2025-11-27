d51ff26522de15c2bafce182c4c4ab02dcda1d8e
excelapplication <excel.application09@gmail.com>
2025-11-27 16:12:57 +0530

    Update CicdApplication.java

--- src/main/java/com/java/cicd/CicdApplication.java
+++ src/main/java/com/java/cicd/CicdApplication.java
@@ -11,7 +11,7 @@ public class CicdApplication {
 
 	@GetMapping("/hello")
 	public String name() {
-		return"jgifdooi";
+		return"jgifdooihgrshydhfchgf";
 	}
 	
 	public static void main(String[] args) {
